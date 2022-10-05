0c938fa06184c60429ed2971c2f93335c62b7819
Anton Mushnin <anton.mushnin@gmail.com>
2022-10-05 11:11:36 +0300

    prettier settings fixed

--- src/model/Operators.ts
+++ src/model/Operators.ts
@@ -1,16 +1,16 @@
-import { UnaryOperator, BinaryOperator } from "./Operator";
+import { UnaryOperator, BinaryOperator } from './Operator';
 
 const unaryOperators = new Map([
-  ["-", new UnaryOperator("-", (a) => -a)],
-  ["\u221A", new UnaryOperator("\u221A", Math.sqrt)],
+  ['-', new UnaryOperator('-', (a) => -a)],
+  ['\u221A', new UnaryOperator('\u221A', Math.sqrt)],
 ]);
 
 const binaryOperators = new Map([
-  ["+", new BinaryOperator("+", (a, b) => a + b, 2)],
-  ["-", new BinaryOperator("-", (a, b) => a - b, 2)],
-  ["*", new BinaryOperator("*", (a, b) => a * b, 1)],
-  ["/", new BinaryOperator("/", (a, b) => a / b, 1)],
-  ["%", new BinaryOperator("%", (a, b) => (a / 100) * b, 1)],
+  ['+', new BinaryOperator('+', (a, b) => a + b, 2)],
+  ['-', new BinaryOperator('-', (a, b) => a - b, 2)],
+  ['*', new BinaryOperator('*', (a, b) => a * b, 1)],
+  ['/', new BinaryOperator('/', (a, b) => a / b, 1)],
+  ['%', new BinaryOperator('%', (a, b) => (a / 100) * b, 1)],
 ]);
 
 const operators = [...unaryOperators.keys(), ...binaryOperators.keys()];
--- src/model/Parser.ts
+++ src/model/Parser.ts
@@ -1,22 +1,22 @@
-import { operators, unaryOperators, binaryOperators } from "./Operators";
-const parentheses = ["(", ")"];
+import { operators, unaryOperators, binaryOperators } from './Operators';
+const parentheses = ['(', ')'];
 const symbols = [...parentheses, ...operators];
 
 function isDigit(c: string): boolean {
-  return (c >= "0" && c <= "9") || c === ".";
+  return (c >= '0' && c <= '9') || c === '.';
 }
 
 function expressionToArray(expression: string): string[] {
   const result: string[] = [];
   for (let i = 0; i < expression.length; i++) {
     const c = expression[i];
-    if (c === " ") {
+    if (c === ' ') {
       continue;
     }
     if (symbols.indexOf(c) > -1) {
       result.push(c);
     } else if (isDigit(c)) {
-      let n = "";
+      let n = '';
       do {
         n += expression[i];
         i += 1;
@@ -34,10 +34,10 @@ function indexOfClosingParenthes(expression: string[]): number {
   let i = 1;
   let count = 1;
   while (i < expression.length) {
-    if (expression[i] === "(") {
+    if (expression[i] === '(') {
       count += 1;
     }
-    if (expression[i] === ")") {
+    if (expression[i] === ')') {
       count -= 1;
     }
     if (count === 0) {
@@ -45,12 +45,12 @@ function indexOfClosingParenthes(expression: string[]): number {
     }
     i += 1;
   }
-  throw new Error("Illegal parenthes");
+  throw new Error('Illegal parenthes');
 }
 
 function getValue(expression: string[]): [number, number] {
-  console.log("getValue", expression);
-  if (expression[0] === "(") {
+  console.log('getValue', expression);
+  if (expression[0] === '(') {
     const closingParenthes = indexOfClosingParenthes(expression);
     return [calc(expression.slice(1, closingParenthes)), closingParenthes + 1];
   }
@@ -89,10 +89,3 @@ function calc(expression: string[]): number {
     throw new Error("can't parse value");
   }
 }
-
-console.log(symbols);
-const s1 = expressionToArray(
-  "-\u221A (2 + 2) * -3 * (2 + 2 + 3 * (\u221A 4  + 2 - 1))"
-);
-console.log(s1);
-console.log(calc(s1));
